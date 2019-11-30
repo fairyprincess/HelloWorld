98c5baecdbeb33388afa4d4308d71b76d139e68b
fairyprincess <gracefv@gmail.com>
2019-11-30 13:38:00 +1300

    Upload #2

--- MyFirstProject/src/myfirst/Main.java
+++ MyFirstProject/src/myfirst/Main.java
@@ -3,7 +3,7 @@ package myfirst;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Output Hello World to the user
 System.out.println("Hello World!");
 	}
 
